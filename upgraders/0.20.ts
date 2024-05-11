1582b3c9c5126be494ba6e16cb768f9af16970c7
Arthur Cosentino <arthurcose@gmail.com>
2024-05-11 15:23:05 -0400

    Upgrade tile_offset -> movement_offset

--- upgraders/0.20.ts
+++ upgraders/0.20.ts
@@ -1,7 +1,12 @@
-import { findFiles } from "../util.ts";
+import { Patch, findFiles, parseLua54, patch, walkAst } from "../util.ts";
 
 export const PREVIOUS_VERSION = "0.13";
 export const NEXT_VERSION = "0.20";
+
+const leafRewrites: { [key: string]: string } = {
+  tile_offset: "movement_offset",
+  set_tile_offset: "set_movement_offset",
+};
 
 export default async function (game_folder: string) {
   const mod_folder = game_folder + "/mods";
@@ -14,9 +19,30 @@ export default async function (game_folder: string) {
 
     let patched_source = source.replace(/TileState\.Hidden/g, "TileState.Void");
 
+    let ast;
+
+    try {
+      ast = parseLua54(patched_source);
+    } catch (e) {
+      console.error(`%cFailed to parse "${path}":\n${e}`, "color: red");
+      continue;
+    }
+
+    const patches: Patch[] = [];
+
+    walkAst(ast, (node) => {
+      const leafRewrite = node.content && leafRewrites[node.content];
+
+      if (leafRewrite) {
+        patches.push(new Patch(node.start, node.end, leafRewrite));
+        return;
+      }
+    });
+
     // apply patches
-    if (source != patched_source) {
+    if (source != patched_source || patches.length > 0) {
       console.log('Patching "' + path + '"...');
+      patched_source = patch(patched_source, patches);
       await Deno.writeTextFile(path, patched_source);
     }
   }
